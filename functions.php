258e91bb529424f98f06b8f4c587eeb1940d4691
Argotto <carranzaragoza@gmail.com>
2025-01-02 18:17:02 +0100

    Reorganize dividing css files

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function storefront_child_enqueue_styles() {
     wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'storefront-child-style', get_stylesheet_uri(), array( 'storefront-style' ) );
+    wp_enqueue_style( 'menu-mobile-style', get_stylesheet_directory_uri() . '/menu-mobile.css', ['storefront-child-style']);
 }
 add_action( 'wp_enqueue_scripts', 'storefront_child_enqueue_styles' );
 
--- iledysile-js.js
+++ iledysile-js.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const nav = document.querySelector(".storefront-primary-navigation");
-    const subMenu = document.querySelector(".sub-menu");
+    const subMenu = document.querySelector(".primary-navigation .sub-menu");
 
     // Escucha el evento de scroll
     window.addEventListener("scroll", function () {
         if (window.scrollY > 125) { //El num. son los píxeles que hay que bajar para que cambie a scrolled
             nav.classList.add("scrolled");
             subMenu.classList.add("scrolled");
+            console.log("Hola");
         } else {
             nav.classList.remove("scrolled");
             subMenu.classList.remove("scrolled");
