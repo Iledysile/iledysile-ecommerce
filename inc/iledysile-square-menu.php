654347a3637c90c3e8e2fff3cc2ae7dcfa4256b8
Argotto <carranzaragoza@gmail.com>
2025-02-11 11:56:45 +0100

    Adjusting artistic square menu css

--- inc/iledysile-square-menu.php
+++ inc/iledysile-square-menu.php
@@ -3,18 +3,31 @@
 function iledysile_add_square_menu() {
     ?>
     <div id="iledysile-square-menu-overlay" class="iledysile-menu-overlay"></div>
+    
     <div id="iledysile-square-menu" class="iledysile-square-menu">
-        <nav class="iledysile-square-menu-items">
-            <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'iledysile-mobile-menu',
-                    'menu_class'     => 'iledysile-menu',
-                    'container'      => false,
-                )
-            );
-            ?>
-        </nav>
+        <div class="iledysile-square-menu-top">
+            <div class="iledysile-square-menu-top-left">
+                <div class="iledysile-square-menu-logo">
+                    <span class="iledysile-logo">Île d'Ysile</span>
+                </div>
+                <nav class="iledysile-square-menu-items">                    
+                    <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'iledysile-mobile-menu',
+                                'menu_class'     => 'iledysile-menu',
+                                'container'      => false,
+                            )
+                        );
+                    ?>
+                </nav>   
+            </div>
+            <div class="iledysile-square-menu-top-right"></div>
+        </div>
+        <div class="iledysile-square-menu-bottom">
+            <div class="iledysile-square-menu-bottom-left"></div>
+            <div class="iledysile-square-menu-bottom-right"></div>
+        </div>
     </div>
     <button class="iledysile-square-menu-button">
     </button>
