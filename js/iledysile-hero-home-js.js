bf20a582bff4b486ae090dc95c78dfed90f11c79
Argotto <carranzaragoza@gmail.com>
2025-05-23 23:00:17 +0200

    Styling everywhere

--- js/iledysile-hero-home-js.js
+++ js/iledysile-hero-home-js.js
@@ -1,32 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var isAnimationOk = window.matchMedia('(prefers-reduced-motion: no-preference)').matches;
-    var scrub = true;
+// Función para mostrar el spinner al cargar la página
+window.addEventListener('load', function() {
+    const spinner = document.getElementById('loading-spinner');
+    setTimeout(() => {
+        spinner.style.opacity = '0';
+        setTimeout(() => {
+            spinner.style.display = 'none'; 
+        }, 300); // Tiempo para que ocurra la transición de opacidad
+    }, 600); // Retardo en ms para asegurarnos de que la página cargue correctamente
+});
 
-    if (isAnimationOk) {
-        setupAnimations();
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const isHome = window.location.pathname === '/' || window.location.pathname === '/index.php';
+  if (!isHome) return;
 
-    function setupAnimations() {
-        gsap.to(".hero__title-bg--front", {
-            color: 'transparent', // asegúrate que sea transparente
-            "-webkit-text-stroke": "1px rgb(255, 106, 105)", // el borde que quieres
-            scale: 2,
-            scrollTrigger: {
-                trigger: ".hero",
-                start: "top top",
-                end: "bottom 75%",
-                scrub: scrub,
-            }
-        });
+  const hero1 = document.querySelector('.iledysile-hero-image');
+  const hero2 = document.querySelector('.iledysile-hero-image-2');
+  if (!hero1 || !hero2) return;
 
-        gsap.to(".hero__title-bg--back", {
-            yPercent: 100,
-            scrollTrigger: {
-                trigger: ".hero",
-                start: "top top",
-                end: "bottom 75%",
-                scrub: scrub,
-            }
-        });
-    }
+  // Obtener estado actual de la imagen mostrada
+  const currentHero = sessionStorage.getItem('currentHero') || '1';
+
+  if (currentHero === '1') {
+    hero1.classList.remove('hidden');
+    hero2.classList.add('hidden');
+    sessionStorage.setItem('currentHero', '2');
+  } else {
+    hero1.classList.add('hidden');
+    hero2.classList.remove('hidden');
+    sessionStorage.setItem('currentHero', '1');
+  }
 });
