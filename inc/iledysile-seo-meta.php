0c59e7386f68dffa4963c1475310879eadb46dff
Argotto <carranzaragoza@gmail.com>
2025-07-03 07:02:00 +0200

    seo meta and shop text seo

--- inc/iledysile-seo-meta.php
+++ inc/iledysile-seo-meta.php
@@ -2,11 +2,11 @@
 // Añadir meta description dinámico
 function custom_meta_tags() {
     if ( is_front_page() ) {
-        echo '<meta name="description" content="Handgemachte, hochwertige Lingerie für Frauen, hergestellt in der Schweiz. Île d&#39;Ysile steht für einzigartige, bequeme, lokal produzierte Lingerie ohne Bügel. Kostenloser Versand schweizweit." />' . "\n";
+        echo '<meta name="description" content="Handgemachte, bequeme Lingerie aus der Schweiz. Île d&#39;Ysile steht für einzigartige, lokal produzierte Lingerie ohne Bügel. Kostenloser Versand." />' . "\n";
         echo '<meta name="keywords" content="Lingerie, handgemacht, Schweiz, hochwertige Materialien, lokal produziert, bequem, nachhaltig" />' . "\n";
         echo '<meta name="author" content="Île d&#39;Ysile" />' . "\n";
     } elseif ( is_shop() ) {
-        echo '<meta name="description" content="Handgemachte Lingerie aus der Schweiz - Echt. Fein. Frei. Für Frauen, die ihre Einzigartigkeit und innere Stimmigkeit leben. Nachhaltig und lokal produziert." />' . "\n";
+        echo '<meta name="description" content="Handgemachte Lingerie aus der Schweiz – Echt. Fein. Frei. Einzigartig und nachhaltig lokal produziert." />' . "\n";
         echo '<meta name="keywords" content="Lingerie Shop, handgemachte Lingerie, Schweiz, nachhaltige Mode, lokal, feminin" />' . "\n";
         echo '<meta name="author" content="Île d&#39;Ysile" />' . "\n";
     } elseif ( is_product() ) {
