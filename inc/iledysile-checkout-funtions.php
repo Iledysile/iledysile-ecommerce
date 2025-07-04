f00f7cdd3ef411a1139dbc8b0c1192186712042a
Argotto <carranzaragoza@gmail.com>
2025-07-04 18:12:27 +0200

    Change checkout order recieved text

--- inc/iledysile-checkout-funtions.php
+++ inc/iledysile-checkout-funtions.php
@@ -2,18 +2,14 @@
 
 // Función para establecer el país por defecto en la página de pago
 add_action('woocommerce_add_to_cart' , 'set_country_befor_cart_page'); 
-
 function set_country_befor_cart_page(){
 
     WC()->customer->set_billing_country('CH');
     WC()->customer->set_shipping_country('CH');
 }
 
-// Función para cambiar el texto al realizar el pedido
-add_filter( 'gettext', 'custom_thank_you_message', 20, 3 );
-function custom_thank_you_message( $translated_text, $text, $domain ) {
-    if ( $domain === 'woocommerce' && $text === 'Vielen Dank. Ihre Bestellung ist eingegangen.' ) {
-        $translated_text = 'VIELEN DANK. DEINE BESTELLUNG IST EINGEGANGEN.';
-    }
-    return $translated_text;
+add_filter( 'woocommerce_thankyou_order_received_text', 'custom_thankyou_text', 10, 2 );
+function custom_thankyou_text( $thank_you_text, $order ) {
+    // Versión tuteo del texto original:
+    return 'Vielen Dank. Deine Bestellung ist eingegangen.';
 }
